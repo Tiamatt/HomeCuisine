9675e27cff06c2748f79d16a6e28cb612e64239e
tiamatt <samira.yusifova@gmail.com>
2018-10-01 16:22:02 -0400

    Initial - create basic app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { UserComponent } from './routes/user/user.component';
+import { AdminComponent } from './routes/admin/admin.component';
+import { GeneralComponent } from './routes/general/general.component';
+import { ContentComponent } from './layout/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    UserComponent,
+    AdminComponent,
+    GeneralComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
