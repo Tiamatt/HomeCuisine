cfe3506d43045485ef1c58c98f003c03a39265ea
tiamatt <samira.yusifova@gmail.com>
2018-10-02 00:10:00 -0400

    Header - design and style Header component

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  collapsed = false;
+  toggleCollapsed(): void {
+    this.collapsed = !this.collapsed;
+  }
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
