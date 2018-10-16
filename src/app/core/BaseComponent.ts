305b71bbb56e8a983f896ef4c86403b503137b1a
tiamatt <samira.yusifova@gmail.com>
2018-10-15 23:09:05 -0400

    BaseComponent - add nullOrWhiteSpaceValidator

--- src/app/core/BaseComponent.ts
+++ src/app/core/BaseComponent.ts
@@ -1,5 +1,7 @@
 import { GeneralService } from './../shared/services/general.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { FormControl } from '@angular/forms';
+
 export class BaseComponent {
     constructor() {
     }
@@ -8,4 +10,17 @@ export class BaseComponent {
         let regexp = (isIcludeZero) ? /^\+?(0|[1-9]\d*)$/ :  /^\+?[1-9]\d*$/;
         return regexp.test(stringNumber);
     }
+
+    // ---------------- BEGIN: VALIDATORS ----------------------- //
+    
+    nullOrWhiteSpaceValidator(control: FormControl):{[s: string]: boolean} {
+        if(control.value == null 
+            || control.value.length === 0 
+            || (control.value.trim().length === 0) ){
+                return {'nullOrWhiteSpace': true};
+            }
+            return null;
+    }
+
+    // ---------------- END: VALIDATORS ----------------------- //
 }
